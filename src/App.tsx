39f0be6c2040b7be7fdce3574e5c33e128283029
Rayane Slimani <rayane1.slimani@epitech.eu>
2020-01-11 17:11:22 +0100

    push file

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import Game from './components/game/Game'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from './img/Life.gif'
+export interface appProps{}
+export interface appState{
+    start:boolean
 }
 
-export default App;
+export default class App extends React.Component<appProps, appState> {
+    constructor(props:appProps) {
+        super(props);
+        this.state = {
+        start:false
+        }
+    }
+    render(){
+        return (
+            <div className="App">
+            {!this.state.start
+            ?(<header className="App-header">
+                  <h1 className="display-2">Le jeu de la vie</h1>
+                  <img src={logo} alt="logo"/>
+                  <button className="btn" onClick={()=>this.setState({start:true})}>Start</button>
+              </header>)
+            :(
+                <Game/>
+            )}
+            </div>
+        )
+    }
+}
