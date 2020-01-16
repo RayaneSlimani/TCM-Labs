56705e8e64def9e128f89a73470db37cc4993b16
Rayane Slimani <rayane1.slimani@epitech.eu>
2020-01-16 14:52:56 +0100

    cypress test algo

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,5 @@
-describe('My First Test', function() {
-    it('Le jeu de la vie', function() {
+describe('Le jeu de la vie Test', function() {
+    it('Test App + Algo', function() {
     cy.visit('localhost:3000')
     cy.contains('Start').click()
     cy.contains('Random').click()
@@ -22,5 +22,26 @@ describe('My First Test', function() {
     cy.get('[style="left: 421px; top: 321px; width: 19px; height: 19px;"]')
     cy.get('[style="left: 401px; top: 321px; width: 19px; height: 19px;"]')
     cy.get('[style="left: 381px; top: 321px; width: 19px; height: 19px;"]')
+    cy.get('.button-div > :nth-child(4)').click()
+
+
+    cy.get('.Board').click(405,350)
+    cy.get('.Board').click(425,350)
+
+
+    cy.get('.Board').click(445,315)
+    cy.get('.Board').click(445,335)
+    cy.get('.Board').click(385,315)
+    cy.get('.Board').click(385,335)
+    cy.get('.button-div > :nth-child(3)').click()
+    cy.get('[style="left: 381px; top: 321px; width: 19px; height: 19px;"]')
+    cy.get('[style="left: 441px; top: 321px; width: 19px; height: 19px;"]')
+    cy.get('[style="left: 401px; top: 341px; width: 19px; height: 19px;"]')
+    cy.get('[style="left: 421px; top: 341px; width: 19px; height: 19px;"]')
+    cy.get('.button-div > :nth-child(3)').click()
+    cy.get('[style="left: 401px; top: 321px; width: 19px; height: 19px;"]')
+    cy.get('[style="left: 421px; top: 321px; width: 19px; height: 19px;"]')
+    cy.get('[style="left: 401px; top: 341px; width: 19px; height: 19px;"]')
+    cy.get('[style="left: 421px; top: 341px; width: 19px; height: 19px;"]')
     })
   })
